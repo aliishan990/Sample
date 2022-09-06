a5153d6c4ee18dd6745131aa78ce6d46a05a9a1b
Ishan Ali <ishan.ali@girmiti.com>
2022-09-06 11:00:29 +0530

    First commit

---
+++ src/test/java/Testclass_1.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class Testclass_1 {
+
+    @Test
+    public void tesatcase_1(){
+        System.out.println("Testcase one result");
+    }
+}
---
+++ src/test/java/Testclass_2.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class Testclass_2 {
+
+    @Test
+    public void tesatcase_2(){
+        System.out.println("Testcase two result");
+    }
+}
