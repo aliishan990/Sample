c805d7ad25a03af2744229a86086c57fe8fbfbfe
Ishan Ali <ishan.ali@girmiti.com>
2022-09-07 21:49:49 +0530

    Typo correction

--- src/test/java/Testclass_1.java
+++ src/test/java/Testclass_1.java
@@ -3,7 +3,7 @@ import org.testng.annotations.Test;
 public class Testclass_1 {
 
     @Test
-    public void tesatcase_1(){
+    public void testcase_1(){
         System.out.println("Testcase one result");
     }
 }
